822b342a70eeb941889c7247cb18a3adc5a6ede8
David Tippett <dtippett@regent.edu>
2018-06-11 12:20:11 -0400

    Probably All Dependencies

---
+++ nlu_model.py
@@ -0,0 +1,18 @@
+from rasa_nlu.training_data import load_data
+from rasa_nlu import config
+from rasa_nlu.model import Trainer
+from rasa_nlu.model import Metadata, Interpreter
+
+def train_nlu(data, nlu_config, directory):
+  training_data = load_data(data)
+  trainer = Trainer(config.load(nlu_config))
+  trainer.train(training_data)
+  model_directory = trainer.persist(directory, fixed_model_name = 'nlu')
+
+def run_nlu ():
+  interpreter = Interpreter.load('./models/nlu/default/nlu')
+  print(interpreter.parse(u"Can you take me to Regent"))
+
+if __name__ == '__main__':
+  #train_nlu('./data/data.json', 'config_spacy.json', './models/nlu')
+  run_nlu()

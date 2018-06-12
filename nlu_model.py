0a6d3d7fa96bc11dc299da1dc6c128cfad2500da
David Tippett <dtippett@regent.edu>
2018-06-12 16:48:55 -0400

    removed trining from running nlu file

--- nlu_model.py
+++ nlu_model.py
@@ -1,15 +1,8 @@
 from rasa_nlu.model import Metadata, Interpreter
 
-def train_nlu(data, nlu_config, directory):
-  training_data = load_data(data)
-  trainer = Trainer(config.load(nlu_config))
-  trainer.train(training_data)
-  model_directory = trainer.persist(directory, fixed_model_name = 'nlu')
-
 def run_nlu ():
   interpreter = Interpreter.load('./models/nlu/default/nlu')
   print(interpreter.parse(u"Id B04010203"))
 
 if __name__ == '__main__':
-  #train_nlu('./data/data.json', 'config_spacy.json', './models/nlu')
   run_nlu()

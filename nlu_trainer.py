9594c6b1bf0fa763f55ef313d99f9c919f775aba
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2018-06-27 11:24:58 -0400

    Lots of additional documentation and a readme doc

--- nlu_trainer.py
+++ nlu_trainer.py
@@ -4,10 +4,22 @@ from rasa_nlu.model import Trainer
 from rasa_nlu.model import Metadata, Interpreter
 
 def train_nlu(data, nlu_config, directory):
+  # Loads the intent training data
   training_data = load_data(data)
+
+  # Creates an instance of the Trainer using the nlu_config
   trainer = Trainer(config.load(nlu_config))
+
+  # Starts the training
   trainer.train(training_data)
-  model_directory = trainer.persist(directory, fixed_model_name = 'nlu')
+
+  # Saves the trained model in the specified directory with the specified name
+  trainer.persist(directory, fixed_model_name = 'nlu')
 
 if __name__ == '__main__':
+  # Calls the trainer function
+  # Takes in:
+  # - Training data file
+  # - Spacy trainer config file
+  # - Output directory
   train_nlu('./data/data.json', 'config_spacy.json', './models/nlu')

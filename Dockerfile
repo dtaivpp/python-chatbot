2c45f987631549ea70e65e69588ca58bab1cc9c6
Daniel Hines <dhines@regent.edu>
2018-08-27 13:54:09 -0400

    Changed docker config, added logging, using environment variable for key

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-FROM python:3.6-alpine3.7
+FROM python:3.6 as build
 WORKDIR /usr/src/app
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 RUN python -m spacy download en_core_web_sm
+RUN pip install pip==9.0.3
 RUN python -m spacy link en_core_web_sm en -f
 COPY . .
+EXPOSE 5004
 CMD [ "python", "./slack_handler.py" ]
--- slack_handler.py
+++ slack_handler.py
@@ -3,23 +3,21 @@ from rasa_core.channels.slack import SlackInput
 from rasa_core.agent import Agent
 from rasa_core.interpreter import RegexInterpreter
 from load_bot import run_bot
-import json
+import os
 
 def run_slack_bot():
+  print("Loading agent...")
   # load your trained agent
   agent = run_bot()
-  filename = "slack_creds.json"
 
-  #Read JSON data into the datastore variable
-  if filename:
-    with open(filename, 'r') as f:
-        credentials = json.load(f)
+  print("Agent loaded.")
 
   input_channel = SlackInput(
-    slack_token= credentials['token'],  # this is the `bot_user_o_auth_access_token`
+    slack_token= os.environ['SLACK_TOKEN'],  # this is the `bot_user_o_auth_access_token`
     slack_channel ="augustine"  # the name of your channel to which the bot posts (optional)
   )
 
+  print("Starting server on port 5004 at /app")
   agent.handle_channel(HttpInputChannel(5004, "/app", input_channel))
 
 if __name__ == "__main__":

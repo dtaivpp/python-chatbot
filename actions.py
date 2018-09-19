60d98377622329c14cfbeeb40bcda3b7b699f9dc
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2018-09-19 14:39:39 -0400

    Added a forms example and a regex example

--- actions.py
+++ actions.py
@@ -2,10 +2,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
 from rasa_core.actions.action import Action
+from rasa_core.actions.forms import FormAction
+from rasa_core.actions.Action import EntityFormField
 from rasa_core.events import SlotSet
 # from requests_ntlm import HttpNtlmAuth
 import requests
 import json
+import re
 
 # These are custom actions for the Chatbot
 # It accepts an action from RASA.Core
@@ -63,16 +66,45 @@ def Get_Name(erpid):
   # get data for the users
   return "Jeff" 
 
-""" class Action_RegexCheck(Acton):
+
+class ActionFillApp(FormAction):
+
+    RANDOMIZE = True
+
+    @staticmethod
+    def required_fields():
+        return [
+            EntityFormField("f_name", "f_name"),
+            EntityFormField("l_name", "l_name"),
+            EntityFormField("phone_number", "phone_number")
+        ]
+
+    def name(self):
+        return 'action_search_restaurants'
+
+    def submit(self, dispatcher, tracker, domain):
+        results = ""
+        #results = RestaurantAPI().search(
+        #    tracker.get_slot("cuisine"),
+        #    tracker.get_slot("people"),
+        #    tracker.get_slot("vegetarian"))
+        return [SlotSet("search_results", results)]
+
+
+class ActionRegexCheck(Action):
+
   def name(self):
     return 'action_regex_check'
   
   def run (self, dispatcher, tracker, domain):
+    pattern = re.compile(r"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$")
+
+    text_to_check = ""
     text_to_check = tracker.get_slot('input')
-    
-    if(regex.match(text_to_check)):
+
+    if(pattern.match(text_to_check)):
       dispatcher.utter_message("It matched!")
       return [SlotSet('input', text_to_check if text_to_check is not None else '')]
 
     dispatcher.utter_message("That is not a valid input.")
-    return[SlotSet('input', '')] """
+    return[SlotSet('input', '')] 
